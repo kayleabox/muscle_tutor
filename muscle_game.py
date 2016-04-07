3c9df6a7a04235ec3420eadc2ac95d9104460c1b
kaylea <kayleabox09@gmail.com>
2016-04-07 17:50:53 -0500

    third commit. put the functions back in the muscle_game file.  program working again.

--- muscle_game.py
+++ muscle_game.py
@@ -1,7 +1,7 @@
 # this program will help the user learn the muscles of the body,
 # their attachments and functions
 import random
-import muscle_functions_group1
+#import muscle_functions_group1
 
 #create the structure of the quiz
 #generate random questions about the muscles in a certain part of the body
@@ -32,11 +32,11 @@ def main():
 
     print('would you like to take a quiz about muscles?')
     print('enter any key to continue or q to quit.')
-    play = input('>')
+    play = input('> ')
     selection = 1
     while play != 'q':
 
-        #menu()
+        menu()
         region_to_test = get_choice()
 
         muscles = random.sample(arm_function.keys(),3)
@@ -76,5 +76,20 @@ def main():
                 exit()
             else:
                 print('choose a valid option')
+
+def menu():
+    print('menu options\n')
+    print('__________________\n')
+    print('2)head and neck\n')
+    print('1)arm and shoulder\n')
+    print('3)hand\n')
+    print('4)thorax and spine\n')
+    print('5)hip\n')
+    print('6)leg\n')
+    print('7)foot\n')
+
+def get_choice():
+    choice = input('what part of the body would you like to work on?')
+    return choice
 
 main()

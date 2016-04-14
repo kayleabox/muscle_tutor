342c551dece4e6eef6f3349ac262c2863eedba0b
kaylea <kayleabox09@gmail.com>
2016-04-14 02:31:29 -0500

    eighth commit. need to find a way to prevent duplicate answer choices. import question_gen_functions is still not working.

--- question_gen_functions.py
+++ question_gen_functions.py
@@ -3,10 +3,14 @@ def gen_rand_question(muscle_dict):
     muscles = random.sample(muscle_dict.keys(),3)
     options = muscle_dict[muscles[0]], muscle_dict[muscles[1]], muscle_dict[muscles[2]]
     question = random.choice(muscles)
-    #muscle_dict.pop(question)
+    while question in already_asked:
+        muscles = random.sample(muscle_dict.keys(), 3)
+        options = muscle_dict[muscles[0]], muscle_dict[muscles[1]], muscle_dict[muscles[2]]
+        question = random.choice(muscles)
+    already_asked.append(question)
     correct_answer = muscle_dict[question]
     return muscles, options, question, correct_answer
-
+    """still need to figure out how to make sure answer choices are not duplicates"""
 
 
 

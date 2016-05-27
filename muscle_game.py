54b00fcadca753684cd57f7bba5830031b5a3f90
kaylea <kayleabox09@gmail.com>
2016-05-26 22:39:53 -0500

    commit sixteen. file is still incomplete. program is reading from the file and creating dictionaries for each region of the body. adjusted ask_question fun to only use the action as an option in the question. program is running. generates questions and correct answers.

--- muscle_game.py
+++ muscle_game.py
@@ -4,7 +4,7 @@ import random
 import menu_functions as menu
 import ask_question_fun as ask_question
 import xml.etree.cElementTree as ElementTree
-
+import xml_functions
 # create the structure of the quiz
 # generate random questions about the muscles in a certain part of the body
 # use a dictionary to store the questions and answers
@@ -22,37 +22,61 @@ for child in root:
     print(child.tag, child.attrib)
 for muscle in root.iter('muscle'):
     print(muscle.attrib)
+#finds muscles and there subbranches(origin, insertion, action)
 for muscle in root.iter('muscle'):
     origin = muscle.find('origin').text
     insertion = muscle.find('insertion').text
     action = muscle.find('action').text
     name = muscle.get('name')
     print("muscle = ", name, "action = ", action, "origin = ", origin, "insertion = ", insertion)
-
-arm_function = {'biceps brachii': 'flex the elbow', 'deltoid': 'flex shoulder', 'triceps brachii': 'extension of elbow'}
-thorax_spine_function = {'rectus abdominis': 'flex the spine', 'longissimus': 'extension of spine',
-                         'multifidi': 'rotate to opposite', 'rotatores': 'rotation to opposite'}
-head_neck_function = {'scm': 'flex neck laterally to same side', 'scalenes': 'elevate ribs',
-                      'masseter': 'elevate mandible', 'temporalis': 'elevate mandible'}
-leg_function = {'rectus femorus': '1', 'adductor magnus': '2', 'vastus lat': '3', 'vastus int': '4', 'vastus med': '5',
-                'biceps fem': '6'}
-hand_function = {'7':'7', '6':'6', '5':'5', '3':'6','4':'5', '8':'7' }
-feet_function = {}
-hip_function = {'glute max': '1', 'glute med': '2', 'glute min': '3', 'piriformis': '4', 'quadratus fem': '5'}
-arm_origin = {}
-thorax_spine_origin = {}
-head_neck_origin = {}
-leg_origin = {}
-hand_origin = {}
-feet_origin = {}
-hip_origin = {}
-arm_insertion = {}
-thorax_spine_insertion = {}
-head_neck_insertion = {}
-leg_insertion = {}
-hand_insertion = {}
-feet_insertion = {}
-hip_insertion = {}
+#this gets the name of the item
+for element in tree.iterfind('thorax_and_spine/muscle'):
+    print(element.tag, element.attrib)
+#gets specific muscle
+for element in tree.iterfind('thorax_and_spine/muscle[@name="trapezius"]'):
+    print(element.tag, element.attrib)
+#head_and_neck dict
+head_and_neck = {}
+region = 'head_and_neck/muscle'
+xml_functions.fill_dict(head_and_neck, region)
+print(head_and_neck.keys())
+print(head_and_neck.values())
+#create arm dictionary
+arm = {}
+region = 'arm/muscle'
+xml_functions.fill_dict(arm, region)
+print(arm.keys())
+print(arm.values())
+#try to create dictionary thorax_and_spine
+thorax_and_spine={}
+region='thorax_and_spine/muscle'
+xml_functions.fill_dict(thorax_and_spine, region)
+print(thorax_and_spine.keys())
+print(thorax_and_spine.values())
+#create leg dictionary
+leg= {}
+region = 'leg/muscle'
+xml_functions.fill_dict(leg, region)
+print(leg.keys())
+print(leg.values())
+#create hand dictionary
+hand= {}
+region = 'hand/muscle'
+xml_functions.fill_dict(hand, region)
+print(hand.keys())
+print(hand.values())
+#create feet dictionary
+feet= {}
+region = 'feet/muscle'
+xml_functions.fill_dict(feet, region)
+print(feet.keys())
+print(feet.values())
+#create hip/pelvis dictionary
+pelvis = {}
+region = 'pelvis/muscle'
+xml_functions.fill_dict(pelvis, region)
+print(pelvis.keys())
+print(pelvis.values())
 
 dup_option = []
 already_asked = []
@@ -78,38 +102,38 @@ def main():
         while region_to_test != QUIT:
             region_to_test = menu.get_choice()
             if region_to_test == HEAD_NECK:
-                for muscle in range(0, len(head_neck_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(head_neck_function, already_asked)
+                for muscle in range(0, len(head_and_neck)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(head_and_neck, already_asked)
                     #ask_question.ask_question(question, options, correct_answer)
                 del already_asked[:]
             elif region_to_test == ARM_SHOULDER:
-                for muscle in range(0, len(arm_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(arm_function, already_asked)
+                for muscle in range(0, len(arm)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(arm, already_asked)
                     #ask_question.ask_question(question, options, correct_answer) # this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == HAND:
-                for muscle in range(0, len(hand_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(hand_function, already_asked)
+                for muscle in range(0, len(hand)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(hand, already_asked)
                     #ask_question.ask_question(question, options, correct_answer) # this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == THORAX_SPINE:
-                for muscle in range(0, len(thorax_spine_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(thorax_spine_function, already_asked)
+                for muscle in range(0, len(thorax_and_spine)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(thorax_and_spine, already_asked)
                     #ask_question.ask_question(question, options, correct_answer)# this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == HIP:
-                for muscle in range(0, len(hip_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(hip_function, already_asked)
+                for muscle in range(0, len(pelvis)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(pelvis, already_asked)
                     #ask_question.ask_question(question, options, correct_answer) # this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == LEG:
-                for muscle in range(0, len(leg_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(leg_function, already_asked)
+                for muscle in range(0, len(leg)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(leg, already_asked)
                     #ask_question.ask_question(question, options, correct_answer) # this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == FOOT:
-                for muscle in range(0, len(feet_function)):
-                    muscles, options, question, correct_answer = ask_question.gen_rand_question(feet_function, already_asked)
+                for muscle in range(0, len(feet)):
+                    muscles, options, question, correct_answer = ask_question.gen_rand_question(feet, already_asked)
                     #ask_question.ask_question(question, options, correct_answer) # this into the gen_rand_question function
                 del already_asked[:]
             elif region_to_test == QUIT:
